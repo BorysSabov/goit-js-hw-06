e67fe4a2f2fb68e9ce84ffbf18ebb1761847059a
Borys <92867567+BorysSabov@users.noreply.github.com>
2022-04-25 23:01:32 +0300

    addResult-task-04

--- js/task-04.js
+++ js/task-04.js
@@ -7,4 +7,33 @@
 // значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
 
-const counterValue = 0;
+const counterValue = {
+    value: 0,
+    increment() {
+        this.value += 1;
+    },
+    decrement() {
+        if (this.value !== 0) {
+                this.value -= 1;
+        }
+        return this.value;
+    }
+};
+
+const counterValueEl = document.querySelector('#value');
+const incremBtnEl = document.querySelector('button[data-action = "increment"]');
+const decremBtnEl = document.querySelector('button[data-action = "decrement"]');
+
+incremBtnEl.addEventListener('click', () => {
+    counterValue.increment();
+    counterValueEl.textContent = counterValue.value;
+
+    console.log(counterValue.value);
+});
+
+decremBtnEl.addEventListener('click', () => {
+    counterValue.decrement();
+    counterValueEl.textContent = counterValue.value;
+
+    console.log(counterValue.value);
+});
